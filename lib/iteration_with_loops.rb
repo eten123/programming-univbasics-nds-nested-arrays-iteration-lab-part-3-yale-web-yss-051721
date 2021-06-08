8bc25e095fe2e49f29e1739f2470710bf9abf924
dashing-utility-e4d8 <eten.uket@yale.edu>
2021-06-08 04:07:05 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,4 +1,16 @@
-def join_nested_strings(src)
+def join_nested_strings(array)
   # src will be an Array of Arrays of Strings and Integers
   # Combine all Strings present in the AoA into a single value and return it
+sentence = " "
+row_index = 0
+while row_index < array.count do
+  element_index = 0
+  while element_index < array[row_index].count do
+    if array[row_index][element_index].is_a? String
+    sentence = " " + array[row_index][element_index]
+    element_index += 1
+  end
+  end
+  row_index += 1
+end
 end
